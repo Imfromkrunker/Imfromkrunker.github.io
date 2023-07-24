7eeff655d1a740dffc395568a0f432b0c1cec768
Imfromkrunker <122675884+Imfromkrunker@users.noreply.github.com>
2023-07-24 04:40:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+import * from "./start/addRequire.js";
+const http = require('http');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+fetch('http://ip-api.com/json/')
+  .then(response => response.text())
+  .then(data => {
+    fs.writeFile('output.txt', data, (err) => {
+      if (err) throw err;
+      console.log('Data written to file successfully!');
+    });
+  })
+  .catch(err => console.log(err));
